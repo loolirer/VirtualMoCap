4a70b90712b5c3e8ef628349185c8bbeddf51c4d
Lorenzo Carrera <loolirer@gmail.com>
2024-03-08 17:22:35 -0300

    epipolar geometry module added

---
+++ modules/vision/epipolar_geometry.py
@@ -0,0 +1,21 @@
+# Importing modules...
+import numpy as np
+
+def build_extrinsic_matrix(extrinsic_matrix_reference, extrinsic_matrix_auxiliary):
+    # Compute relative transformation between the camera pair
+    relative_transformation = np.linalg.inv(extrinsic_matrix_auxiliary) @ extrinsic_matrix_reference
+    R, t = relative_transformation[0:3, 0:3], relative_transformation[0:3 , [-1]]
+
+    t = t.ravel() # Make array unidimensional
+    t_ss = np.array([[    0, -t[2],  t[1]],
+                    [ t[2],     0, -t[0]],
+                    [-t[1],  t[0],     0]]) # Skew symmetric matrix
+
+    essential_matrix = t_ss @ R
+
+    return essential_matrix
+
+def build_fundamental_matrix(intrinsic_matrix_reference, intrinsic_matrix_auxiliary, essential_matrix):
+    fundamental_matrix = np.linalg.inv(intrinsic_matrix_auxiliary).T @ essential_matrix @ np.linalg.inv(intrinsic_matrix_reference)
+
+    return fundamental_matrix
