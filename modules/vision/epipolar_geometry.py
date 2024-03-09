f585916ff25d1abbf2575aeb74abd1059b1a5859
Lorenzo Carrera <loolirer@gmail.com>
2024-03-09 17:20:50 -0300

    function rename correction

--- modules/vision/epipolar_geometry.py
+++ modules/vision/epipolar_geometry.py
@@ -1,7 +1,7 @@
 # Importing modules...
 import numpy as np
 
-def build_extrinsic_matrix(extrinsic_matrix_reference, extrinsic_matrix_auxiliary):
+def build_essential_matrix(extrinsic_matrix_reference, extrinsic_matrix_auxiliary):
     # Compute relative transformation between the camera pair
     relative_transformation = np.linalg.inv(extrinsic_matrix_auxiliary) @ extrinsic_matrix_reference
     R, t = relative_transformation[0:3, 0:3], relative_transformation[0:3 , [-1]]
