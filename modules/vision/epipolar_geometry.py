73adccb3b42d70daf94579efbe820ee934421451
Lorenzo Carrera <loolirer@gmail.com>
2024-06-23 14:50:02 -0300

    added decompose_essential_matrix to module

--- modules/vision/epipolar_geometry.py
+++ modules/vision/epipolar_geometry.py
@@ -1,5 +1,6 @@
 # Importing modules...
 import numpy as np
+import cv2
 from scipy.optimize import linear_sum_assignment
 
 def build_essential_matrix(extrinsic_matrix_reference, extrinsic_matrix_auxiliary):
@@ -38,3 +39,50 @@ def epiline_order(blobs_auxiliary, epilines_auxiliary):
     _, new_indices = linear_sum_assignment(distance_matrix)
 
     return blobs_auxiliary[new_indices]
+
+def decompose_essential_matrix(essential_matrix, 
+                               blobs_reference, 
+                               blobs_auxiliary, 
+                               intrinsic_matrix_reference, 
+                               intrinsic_matrix_auxiliary):
+    
+    # Decompose the essential matrix
+    R1, R2, t = cv2.decomposeEssentialMat(essential_matrix)
+    
+    # Possible rotations and translations for the auxiliary camera
+    R_t_options = [[R1,  t],
+                   [R1, -t],
+                   [R2,  t],
+                   [R2, -t]]
+    
+    best_option = None
+
+    # Projection matrix of the first camera
+    P_reference = intrinsic_matrix_reference @ np.hstack((np.eye(3), np.zeros((3, 1))))
+    
+    # Triangulate points and check their validity
+    for option, R_t in enumerate(R_t_options):
+        P_auxiliary = intrinsic_matrix_auxiliary @ np.hstack(R_t)
+
+        triangulated_points_4D = cv2.triangulatePoints(P_reference.astype(np.float32), 
+                                                       P_auxiliary.astype(np.float32), 
+                                                       blobs_reference.T.astype(np.float32), 
+                                                       blobs_auxiliary.T.astype(np.float32))
+        
+        # Normalize homogeneous coordinates and discard last row
+        triangulated_points_3D = (triangulated_points_4D / triangulated_points_4D[-1])[:-1, :]
+
+        # Count how many points have a positive z-coordinate 
+        # This indicates if the point is in front of the camera or not
+        frontal_points = np.count_nonzero(triangulated_points_3D[2] > 0)
+
+        # No markers were shot behind the cameras
+        if frontal_points == triangulated_points_3D.shape[1]:
+            best_option = option # Where all points are frontal points
+
+    # No valid decomposition
+    if best_option is None:
+        return None, None 
+    
+    # Valid decomposition
+    return R_t_options[best_option]
