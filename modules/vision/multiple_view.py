2bac79ffbc26367f83ce8a936ff0bc59427f9d0a
Lorenzo Carrera <loolirer@gmail.com>
2024-06-23 16:53:26 -0300

    added class method for fundamental matrices building

--- modules/vision/multiple_view.py
+++ modules/vision/multiple_view.py
@@ -5,48 +5,50 @@ from modules.vision.camera import *
 from modules.vision.epipolar_geometry import * 
 
 class MultipleView:
-    def __init__(self, cameras):
+    def __init__(self, camera_models):
         # Camera model information
-        self.cameras = cameras
-        self.n_cameras = len(cameras)
+        self.camera_models = camera_models
+        self.n_cameras = len(camera_models)
 
-        # Build all fundamental matrices between camera pairs
+        # Fundamental matrices between each pair
         self.fundamental_matrix = np.array(np.zeros((self.n_cameras, self.n_cameras, 3, 3)))
+        self.build_fundamental_matrices()
 
+    def build_fundamental_matrices(self):
+        # Build all fundamental matrices between camera pairs
         for reference in range(self.n_cameras):
             for auxiliary in range(self.n_cameras):
                 if reference == auxiliary:
                     continue
 
-                E = build_essential_matrix(self.cameras[reference].extrinsic_matrix, self.cameras[auxiliary].extrinsic_matrix)
+                E = build_essential_matrix(self.camera_models[reference].extrinsic_matrix, self.camera_models[auxiliary].extrinsic_matrix)
 
-                F = build_fundamental_matrix(self.cameras[reference].intrinsic_matrix, self.cameras[auxiliary].intrinsic_matrix, E)
+                F = build_fundamental_matrix(self.camera_models[reference].intrinsic_matrix, self.camera_models[auxiliary].intrinsic_matrix, E)
 
                 self.fundamental_matrix[reference][auxiliary] = F
 
-    def triangulate_by_pair(self, pair, undistorted_blobs_pair):
-        # Gathering pair info
-        camera_pair = [self.cameras[pair[0]], self.cameras[pair[1]]]
-        pair_fundamental_matrix = self.fundamental_matrix[pair[0]][pair[1]]
-        
+    def triangulate_by_pair(self, pair, blobs_pair):
         reference, auxiliary = (0, 1) # Naming for the sake of code readability
 
+        # Gathering pair info
+        camera_pair = [self.camera_models[pair[reference]], self.camera_models[pair[auxiliary]]]
+        pair_fundamental_matrix = self.fundamental_matrix[pair[reference]][pair[auxiliary]]
+    
         # Compute epipolar lines
-        epilines_auxiliary = cv2.computeCorrespondEpilines(points=undistorted_blobs_pair[reference], 
+        epilines_auxiliary = cv2.computeCorrespondEpilines(points=blobs_pair[reference], 
                                                            whichImage=1, 
                                                            F=pair_fundamental_matrix).reshape(-1,3)
         
         # Order blobs
-        undistorted_blobs_pair[auxiliary] = epiline_order(undistorted_blobs_pair[auxiliary], epilines_auxiliary)
+        blobs_pair[auxiliary] = epiline_order(blobs_pair[auxiliary], epilines_auxiliary)
 
         # Triangulate markers
         triangulated_points_4D = cv2.triangulatePoints(camera_pair[reference].projection_matrix.astype(np.float32), 
                                                        camera_pair[auxiliary].projection_matrix.astype(np.float32), 
-                                                       undistorted_blobs_pair[reference].T.astype(np.float32), 
-                                                       undistorted_blobs_pair[auxiliary].T.astype(np.float32))
+                                                       blobs_pair[reference].T.astype(np.float32), 
+                                                       blobs_pair[auxiliary].T.astype(np.float32))
         
         # Normalize homogeneous coordinates and discard last row
         triangulated_points_3D = (triangulated_points_4D / triangulated_points_4D[-1])[:-1, :]
 
         return triangulated_points_3D
-        
