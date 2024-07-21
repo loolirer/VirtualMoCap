e4cbd358b87e4e292c940cc1d4dce97c37cf6f8b
Lorenzo Carrera <loolirer@gmail.com>
2024-07-20 22:23:05 -0300

    added bundle adjustment method

--- modules/vision/multiple_view.py
+++ modules/vision/multiple_view.py
@@ -1,5 +1,6 @@
 # Importing modules...
 import numpy as np
+import scipy as sp
 
 from modules.vision.camera import *
 from modules.vision.epipolar_geometry import * 
@@ -54,51 +55,45 @@ class MultipleView:
         return triangulated_points_3D
     
     def calibrate(self, wand_blobs, wand_distances):
-        # Camera pairs
-        camera_ids = np.arange(self.n_cameras)
-        reference = 0 # The 0th camera will be the reference by default
-        pairs = [(reference, ID) for ID in camera_ids[camera_ids != reference]]
-
-        # New reference pose
-        self.camera_models[reference].update_reference(np.eye(4)) 
-
         # Getting wand data
         wand_ratio = (1.0, wand_distances[1] / wand_distances[0]) 
+
+        # Camera pairs
+        camera_ids = np.arange(self.n_cameras)
+        pairs = [(i, j) for i in camera_ids for j in camera_ids[camera_ids != i]] 
+
+        # Dicts for storing the final data
+        relative_poses = {} 
+        triangulated_markers = {}
+
+        # Order collinear blobs
+        all_ordered_blobs_per_frame = []
+        for same_frame_blobs in zip(*wand_blobs):
+            ordered_blobs = [collinear_order(same_camera_blobs, wand_ratio) for same_camera_blobs in same_frame_blobs]
+
+            # Get only ordered blobs that are simultaneously viewed by all cameras
+            if any(O is None for O in ordered_blobs):
+                continue
+
+            all_ordered_blobs_per_frame.append(ordered_blobs)
+        all_ordered_blobs_per_frame = np.array(all_ordered_blobs_per_frame)
 
         for pair in pairs:
             # Getting data from pair
             reference, auxiliary = pair
             
-            # Synchronized blobs for each pair
-            sync_blobs_pair = [wand_blobs[reference], wand_blobs[auxiliary]]
-            
-            # Order collinear blobs
-            ordered_blobs_reference = []
-            ordered_blobs_auxiliary = []
-
-            for sync_blobs_reference, sync_blobs_auxiliary in zip(*sync_blobs_pair):
-                blobs_reference = collinear_order(sync_blobs_reference, wand_ratio)
-                blobs_auxiliary = collinear_order(sync_blobs_auxiliary, wand_ratio)
-
-                if blobs_reference is None or blobs_auxiliary is None:
-                    continue
-
-                ordered_blobs_reference.append(blobs_reference)
-                ordered_blobs_auxiliary.append(blobs_auxiliary)
-
-            ordered_blobs_reference = np.array(ordered_blobs_reference)
-            ordered_blobs_auxiliary = np.array(ordered_blobs_auxiliary)
+            # Synchronized blobs for each camera in pair
+            ordered_blobs_reference_per_frame = all_ordered_blobs_per_frame[:, reference, :, :]
+            ordered_blobs_auxiliary_per_frame = all_ordered_blobs_per_frame[:, auxiliary, :, :]
 
             # Join all ordered blobs in a single matrix
-            all_blobs_reference = np.vstack(ordered_blobs_reference)
-            all_blobs_auxiliary = np.vstack(ordered_blobs_auxiliary)
+            all_blobs_reference = np.vstack(ordered_blobs_reference_per_frame)
+            all_blobs_auxiliary = np.vstack(ordered_blobs_auxiliary_per_frame)
 
             # Estimating and saving the Fundamental Matrix
             F_estimated, mask = cv2.findFundamentalMat(points1=all_blobs_reference, 
                                                        points2=all_blobs_auxiliary, 
                                                        method=cv2.FM_8POINT)
-            
-            self.fundamental_matrix[reference][auxiliary] = F_estimated
 
             # Selecting inlier points    
             inlier_all_blobs_reference = all_blobs_reference[mask.ravel() == 1]
@@ -106,8 +101,8 @@ class MultipleView:
 
             mask = np.array([np.prod(flags) for flags in mask.reshape(-1, 3)])
 
-            inlier_blobs_reference = ordered_blobs_reference[mask == 1]
-            inlier_blobs_auxiliary = ordered_blobs_auxiliary[mask == 1]
+            inlier_blobs_reference_per_frame = ordered_blobs_reference_per_frame[mask == 1]
+            inlier_blobs_auxiliary_per_frame = ordered_blobs_auxiliary_per_frame[mask == 1]
 
             # Calculating essential matrix
             E = self.camera_models[auxiliary].intrinsic_matrix.T @ F_estimated @ self.camera_models[reference].intrinsic_matrix
@@ -121,48 +116,150 @@ class MultipleView:
 
             # Check if decomposition worked
             if R is None and t is None:
-                print('> No valid decomposition!')
-                continue
+                print('> Could not find reliable decomposition!')
+                return False # Calibration failed!
 
             # Calculating projection matrices
             # The reference camera will be the reference frame, thus the identity matrix
             P_reference = self.camera_models[reference].intrinsic_matrix @ np.eye(4)[:3, :4]
             P_auxiliary = self.camera_models[auxiliary].intrinsic_matrix @ np.hstack((R, t))
             
-            all_triangulated_points_3D = [] # List of triangulated points
+            all_triangulated_points = [] # List of triangulated points
             all_unscaled_distances = [] # List of all measured distance
             scales = [] # Scale factor for each triangulated frame
 
-            for points_reference_PTS, points_auxiliary_PTS in zip(inlier_blobs_reference, inlier_blobs_auxiliary):
-                triangulated_points_4D = cv2.triangulatePoints(P_reference.astype(np.float32), 
+            for points_reference_per_frame, points_auxiliary_per_frame in zip(inlier_blobs_reference_per_frame, inlier_blobs_auxiliary_per_frame):
+                triangulated_points_h = cv2.triangulatePoints(P_reference.astype(np.float32), 
                                                                P_auxiliary.astype(np.float32), 
-                                                               points_reference_PTS.T.astype(np.float32), 
-                                                               points_auxiliary_PTS.T.astype(np.float32))
+                                                               points_reference_per_frame.T.astype(np.float32), 
+                                                               points_auxiliary_per_frame.T.astype(np.float32))
                 
                 # Normalize homogeneous coordinates and discard last row
-                triangulated_points_3D = (triangulated_points_4D / triangulated_points_4D[-1])[:-1, :]
+                triangulated_points = (triangulated_points_h / triangulated_points_h[-1])[:-1, :]
 
                 # Unscaled distances
-                unscaled_distances = np.array([np.linalg.norm(triangulated_points_3D.T[0] - triangulated_points_3D.T[1]),
-                                               np.linalg.norm(triangulated_points_3D.T[1] - triangulated_points_3D.T[2]),
-                                               np.linalg.norm(triangulated_points_3D.T[0] - triangulated_points_3D.T[2])])
+                unscaled_distances = np.array([np.linalg.norm(triangulated_points.T[0] - triangulated_points.T[1]),
+                                               np.linalg.norm(triangulated_points.T[1] - triangulated_points.T[2]),
+                                               np.linalg.norm(triangulated_points.T[0] - triangulated_points.T[2])])
 
                 # Save values
-                all_triangulated_points_3D.append(triangulated_points_3D)
+                all_triangulated_points.append(triangulated_points)
                 all_unscaled_distances.append(unscaled_distances)
                 scales.append(np.sum(wand_distances) / np.sum(unscaled_distances))
 
             # Mean scale factor
             scale = np.mean(np.array(scales))
 
-            # Saving scaled matrix
+            scaled_triangulated_points = np.hstack(np.array(all_triangulated_points)) * scale
+            triangulated_markers[pair] = scaled_triangulated_points
+
+            # Saving scaled matrices
             extrinsic_matrix_auxiliary = np.vstack((np.hstack((R, t * scale)),
                                                     np.array([0, 0, 0, 1])))
-            pose_auxiliary = np.linalg.inv(extrinsic_matrix_auxiliary)
-            self.camera_models[auxiliary].update_reference(pose_auxiliary)
+            relative_poses[pair] = np.linalg.inv(extrinsic_matrix_auxiliary)
+
+        # Update references
+        reference = 0 # The 0th camera will be the reference by default
+        self.camera_models[reference].update_reference(np.eye(4))
+        for ID in camera_ids[camera_ids != reference]: 
+            self.camera_models[ID].update_reference(relative_poses[(reference, ID)])
 
         # Rebuild fundamental matrices with updated references
         self.build_fundamental_matrices()
+
+        return True # Calibration succeeded!
+
+    def bundle_adjustment(self, wand_blobs, wand_distances, n_observations):
+        # Getting wand data
+        wand_ratio = (1.0, wand_distances[1] / wand_distances[0]) 
+
+        # Order collinear blobs
+        all_ordered_blobs_per_frame = []
+        for same_frame_blobs in zip(*wand_blobs):
+            ordered_blobs = [collinear_order(same_camera_blobs, wand_ratio) for same_camera_blobs in same_frame_blobs]
+
+            # Get only ordered blobs that are simultaneously viewed by all cameras
+            if any(O is None for O in ordered_blobs):
+                continue
+
+            all_ordered_blobs_per_frame.append(ordered_blobs)
+        all_ordered_blobs_per_frame = np.array(all_ordered_blobs_per_frame)
+
+        # All pairs with no repetition
+        camera_ids = np.arange(self.n_cameras)
+        unique_pairs = [(i, j) for i in camera_ids for j in camera_ids[i+1:]]
+
+        # Triangulate points for each pair
+        all_triangulated_points = []
+        for pair in unique_pairs:
+            reference, auxiliary = pair
+
+            triangulated_points = []
+            for blobs_in_frame in all_ordered_blobs_per_frame:
+                triangulated_points.append(self.triangulate_by_pair(pair, [blobs_in_frame[reference],
+                                                                           blobs_in_frame[auxiliary]]))
+
+            triangulated_points = np.hstack(np.array(triangulated_points))
+            all_triangulated_points.append(triangulated_points)
+            
+        all_triangulated_points = np.array(all_triangulated_points)
+        all_ordered_blobs = np.array([np.vstack(np.array(all_ordered_blobs_per_frame)[:, ID]) for ID in camera_ids])
+
+        # Relation between all observations in the capture and the chosen ones
+        total_observations = all_ordered_blobs.shape[1]
+
+        # Get roughly equally spaced observations in time (and hopefully in space)
+        indexes = np.arange(0, total_observations, total_observations//n_observations + 1)
+        pairs_sequence = np.arange(n_observations) % len(unique_pairs)
+        all_ordered_blobs = all_ordered_blobs[:, indexes]
+        all_triangulated_points = np.hstack([all_triangulated_points[p, :, i].reshape(3, -1) for p, i in zip(pairs_sequence, indexes)])
+
+        # Condensing initial guess into initial guess parameter vector
+        rvecs_tvecs = np.array([[cv2.Rodrigues(camera.extrinsic_matrix[:3, :3])[0].flatten(), 
+                                camera.extrinsic_matrix[:3, -1]] for camera in self.camera_models])
+
+        initial_guess = np.hstack((rvecs_tvecs.flatten(), all_triangulated_points.flatten()))
+
+        # Optimizing parameter vector by minimizing cost function using Levenberg–Marquardt algorithm
+        result = sp.optimize.least_squares(fun=total_reprojection_error, 
+                                        x0=initial_guess,
+                                        method='lm',
+                                        args=([camera.intrinsic_matrix for camera in self.camera_models],
+                                                all_ordered_blobs))
+
+        optimized_parameters = np.array(result.x)
+
+        # Retrieving data from optimized parameter vector
+        rvecs_tvecs_adjusted = optimized_parameters[:self.n_cameras * 6].reshape(self.n_cameras, 2, 3)
+        adjusted_extrinsics = [np.hstack((cv2.Rodrigues(rvec)[0], tvec.reshape(3, -1))) for rvec, tvec in rvecs_tvecs_adjusted]
+        adjusted_poses = [np.linalg.inv(np.vstack((E, [0, 0, 0, 1]))) for E in adjusted_extrinsics]
+
+        # Update references
+        for camera, adjusted_pose in zip(self.camera_models, adjusted_poses): 
+            camera.update_reference(adjusted_pose)
+
+        # Rebuild fundamental matrices with updated references
+        self.build_fundamental_matrices()
+
+def total_reprojection_error(optimize, intrinsic_matrices, all_ordered_blobs):
+    n_cameras = len(intrinsic_matrices)
+    rvecs_tvecs = optimize[:n_cameras * 6].reshape(n_cameras, 2, 3)
+    all_triangulated_points = optimize[n_cameras * 6:].reshape(3, -1)
+
+    extrinsic_matrices = [np.hstack((cv2.Rodrigues(rvec)[0], tvec.reshape(3, -1))) for rvec, tvec in rvecs_tvecs]
+    projection_matrices = [I @ E for I, E in zip(intrinsic_matrices, extrinsic_matrices)]
+
+    residuals = []
+    for projection_matrix, all_detected_blobs in zip(projection_matrices, all_ordered_blobs):
+        camera_residuals = reprojection_error(points_to_project=all_triangulated_points, 
+                                              points_in_image=all_detected_blobs,
+                                              projection_matrix=projection_matrix)
+            
+        residuals.append(camera_residuals)
+
+    residuals = np.concatenate(residuals)
+    
+    return residuals
 
 def collinear_order(blobs, ratio):
     # Distances between blobs
