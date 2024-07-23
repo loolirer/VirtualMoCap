52ae716234f6e57b4baa3f79424ae1341716c3be
Lorenzo Carrera <loolirer@gmail.com>
2024-07-23 19:28:15 -0300

    minor correction

--- modules/vision/image_noise.py
+++ modules/vision/image_noise.py
@@ -1,11 +1,11 @@
 # Importing modules...
 import numpy as np
 
-def add_noise(image_gray, snr):
+def add_noise(image_noiseless, snr):
     if snr == np.inf:
-        return image_gray
+        return image_noiseless
     
-    image_noisy = np.random.normal(image_gray.astype(np.float32), image_gray / snr)
+    image_noisy = np.random.normal(image_noiseless.astype(np.float32), image_noiseless / snr)
 
     image_noisy = np.clip(0, 255, image_noisy).astype(np.uint8)
 
