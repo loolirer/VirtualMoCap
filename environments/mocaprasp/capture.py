a163b500622d69d71d12b4c2da096d87ec5bf417
Lorenzo Carrera <loolirer@gmail.com>
2025-06-07 14:59:32 -0300

    timing performance

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -4,18 +4,25 @@ import time
 
 # Initialize camera
 picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration(main={"format": "RGB888", "size": (640, 480)}))
+picam2.configure(
+    picam2.create_video_configuration(main={"format": "RGB888", "size": (640, 480)})
+)
 picam2.start()
 
 # Allow camera to warm up
 time.sleep(1)
 
 while True:
+    start = time.time()
+
     frame = picam2.capture_array()  # Direct NumPy array
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-    cv2.imshow("Gray Feed", gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    finish = time.time()
+
+    print(1 / (finish - start))
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cv2.destroyAllWindows()
