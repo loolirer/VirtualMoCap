9fba29dc733138c7c2bef3d0e5e1219f6eb54a55
Lorenzo Carrera <loolirer@gmail.com>
2025-06-07 16:58:34 -0300

    fix

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -94,21 +94,24 @@ try:
         start = time.time()
         frame = picam2.capture_array()  # Direct NumPy array
         gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        #blobs = detect_blobs(gray)
+        blobs = detect_blobs(gray)
         finish = time.time()
         print(1 / (finish - start))
 
-        #if len(blobs):
-        #    for b in blobs:
-        #        cv2.circle(
-        #            frame,
-        #            center=b.astype(int),
-        #            radius=6,
-        #            color=(0, 0, 255),
-        #            thickness=-1,
-        #        )
+        if len(blobs):
+            for b in blobs:
+                cv2.circle(
+                    frame,
+                    center=b.astype(int),
+                    radius=6,
+                    color=(0, 0, 255),
+                    thickness=-1,
+                )
 
         cv2.imshow("Detection", frame)
+
+        if cv2.waitKey(1) == ord("q"):
+            break
 
 except KeyboardInterrupt:
     pass
