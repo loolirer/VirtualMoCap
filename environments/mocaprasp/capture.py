f726162d149d333ebe4ef04281e563b503dc81b3
Lorenzo Carrera <loolirer@gmail.com>
2025-06-07 14:44:20 -0300

    changed back to picamzero

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -1,21 +1,7 @@
-from picamera2 import Picamera2
-import time
-import cv2
+from picamzero import Camera
+from time import sleep
 
-# Initialize camera
-picam2 = Picamera2()
-picam2.preview_configuration.main.size = (640, 480)
-picam2.preview_configuration.main.format = "RGB888"
-picam2.configure("preview")
-
-# Start the camera
-picam2.start()
-time.sleep(2)  # Sllow sensor to settle
-
-# Capture an image
-image = picam2.capture_array()
-
-# Show the image using OpenCV
-cv2.imshow("Captured Image", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cam = Camera()
+cam.start_preview()
+# Keep the preview window open for 5 seconds
+sleep(5)
