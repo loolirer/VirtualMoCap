a3874e9d4fca706ce095c850fa2ab79dbc5424a1
Lorenzo Carrera <loolirer@gmail.com>
2025-06-09 15:26:40 -0300

    back to yuv

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -13,8 +13,9 @@ from modules.vision.blob_detection import detect_blobs
 
 # Camera setup
 picam2 = Picamera2()
+resolution = (960, 720)
 config = picam2.create_video_configuration(
-    main={"size": (960, 720), "format": "RGB888"}
+    main={"size": resolution, "format": "YUV420"} # Already captures in grayscale
 )
 picam2.configure(config)
 picam2.start()
@@ -39,7 +40,7 @@ shot_counter = 0
 lock = threading.Lock()
 
 
-# ===== GPIO Callback: Capture Only =====
+# GPIO controlled capture callback
 def capture_callback(gpio, level, tick):
     global shot_counter
     with lock:
@@ -47,7 +48,7 @@ def capture_callback(gpio, level, tick):
         shot_counter += 1
 
     timestamp = time.time()
-    frame = picam2.capture_array()
+    frame = picam2.capture_array()[:resolution[1], :resolution[0]]
 
     # Push to processing queue
     frame_queue.put((shot_number, timestamp, frame))
@@ -61,8 +62,7 @@ def process_and_send():
         except queue.Empty:
             continue
 
-        image_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        blobs = detect_blobs(image_gray, area=True)
+        blobs = detect_blobs(frame, area=True)
 
         message = np.append(np.ravel(blobs), [shot_number, timestamp]).astype(
             np.float64
@@ -96,7 +96,7 @@ pi.set_glitch_filter(TRIGGER_PIN, 10000)
 # Register callback on falling edge (level=0)
 cb = pi.callback(TRIGGER_PIN, pigpio.FALLING_EDGE, capture_callback)
 
-# Parameters
+# Clock parameters
 FREQUENCY_HZ = 30  # Desired frequency
 DUTY_CYCLE = 500000  # 50% duty (range: 0–1,000,000)
 
@@ -108,6 +108,7 @@ pi.hardware_PWM(CLOCK_PIN, FREQUENCY_HZ, DUTY_CYCLE)
 print("[INFO] Clock simulation running on GPIO18 → Trigger input on GPIO17")
 print("[INFO] Press Ctrl+C to stop.")
 
+# Stall loop
 try:
     while True:
         time.sleep(0.1)
