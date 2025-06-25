28a0be6ed35f06427a7c0ad7fcea234f21f2d51d
Lorenzo Carrera <loolirer@gmail.com>
2025-06-25 18:21:57 -0300

    kill all camera processes before calling capture

--- environments/mocaprasp/start.sh
+++ environments/mocaprasp/start.sh
@@ -15,5 +15,8 @@ source $HOME/.venv/bin/activate > /dev/null 2>&1 || echo "[ERROR] Venv Activatio
 # Change for development directory for convenience
 cd $HOME/VirtualMoCap/environments/mocaprasp/ > /dev/null 2>&1 || echo "[ERROR] Test Directory Not Found"
 
+# Kill all processes using the camera
+sudo fuser -k /dev/video0
+
 # Run capture script
 python -m capture
