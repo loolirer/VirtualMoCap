b81a6a8323dfafcdb128590a24147020bddc3b0f
Lorenzo Carrera <loolirer@gmail.com>
2025-07-04 11:58:35 -0300

    name window

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -86,7 +86,7 @@ def process_and_send():
         for b in blobs:
             cv2.circle(frame_rgb, center=b[:2].astype(int), radius=5, color=(255, 0, 0), thickness=-1)
 
-        cv2.imshow("", frame_rgb)
+        cv2.imshow("Camera Feed", frame_rgb)
         cv2.waitKey(1)
 
         message = np.append(np.ravel(blobs), [shot_number, timestamp]).astype(
