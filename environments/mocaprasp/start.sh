1833fe099b58b1900960e0835557d49f4c120665
Lorenzo Carrera <loolirer@gmail.com>
2025-07-04 19:59:34 -0300

    refactor

--- environments/mocaprasp/start.sh
+++ environments/mocaprasp/start.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
 
 # Checkout to test branch
-git -C $HOME/VirtualMoCap checkout test > /dev/null 2>&1 || echo "[ERROR] Checkout Failed"
+git -C $HOME/VirtualMoCap checkout test > /dev/null 2>&1 || echo "[ERROR] Checkout failed"
 
 # Get updates, if any, from the remote to local repo
-git -C $HOME/VirtualMoCap pull > /dev/null 2>&1 || echo -e "\n[ERROR] Git Pull Failed"
+git -C $HOME/VirtualMoCap pull > /dev/null 2>&1 || echo -e "\n[ERROR] Git pull failed"
 
 # Start pigpiod daemon for advanced GPIO control
-sudo pigpiod > /dev/null 2>&1 || echo "[ERROR] Pigpio Daemon Failed"
+sudo pigpiod > /dev/null 2>&1 || echo "[ERROR] Pigpio daemon failed"
 
 # Activate python virtual environment
-source $HOME/.venv/bin/activate > /dev/null 2>&1 || echo "[ERROR] Venv Activation Failed"
+source $HOME/.venv/bin/activate > /dev/null 2>&1 || echo "[ERROR] Venv activation failed"
 
 # Change for development directory for convenience
-cd $HOME/VirtualMoCap/environments/mocaprasp/ > /dev/null 2>&1 || echo "[ERROR] Test Directory Not Found"
+cd $HOME/VirtualMoCap/environments/mocaprasp/ > /dev/null 2>&1 || echo "[ERROR] Test directory not found"
 
 # Kill all processes using the camera
-sudo fuser -k /dev/video0 > /dev/null 2>&1 || echo "[ERROR] Could Not Kill Camera Processes"
+sudo fuser -k /dev/video0 > /dev/null 2>&1 || echo "[INFO] Could not kill camera process"
 
 # Run capture script
 python -m capture
