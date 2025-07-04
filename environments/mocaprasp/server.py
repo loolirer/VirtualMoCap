b587f70971c5cb32d561e5a6460a53d3a51e128e
Lorenzo Carrera <loolirer@gmail.com>
2025-07-03 21:39:12 -0300

    update test script

--- environments/mocaprasp/server.py
+++ environments/mocaprasp/server.py
@@ -6,7 +6,7 @@ import time
 try:
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
     server_ip = "0.0.0.0"
-    server_port = 8888
+    server_port = 25565
     server_address = (server_ip, server_port)
     server_socket.bind(server_address)
 
@@ -15,38 +15,43 @@ try:
 except socket.error as err:
     print(f"[ERROR] Socket creation failed with error: {err}")
 
-last_time = 0.0
-last_shot = 0
+client_addresses = []
 
-print("[INFO] Waiting for clients...")
-_, client_address = server_socket.recvfrom(1024)
+
+for ID in range(4):
+    try:
+        client_ip = socket.gethostbyname(f"mocaprasp-client-{ID}.local")
+        client_port = 25565
+        client_address = (client_ip, client_port)
+        print(f"[INFO] Client {ID} found at: {client_address}...")
+        client_addresses.append(client_address)
+
+    except Exception as e:
+        print(e)
+
 
 print("[INFO] Sending capture request...")
-delay = 10
+delay = 5
 capture_time = 15
 message = np.array([delay, capture_time]).astype(np.float32)
 message_bytes = message.tobytes()
-server_socket.sendto(message_bytes, client_address)
+
+for client_address in client_addresses: 
+    server_socket.sendto(message_bytes, client_address)
 
 print(f"[INFO] Receiving messages...")
 try:
     while True:
         message_bytes, address = server_socket.recvfrom(1024)
 
+        print(address)
+
         message = np.frombuffer(message_bytes, dtype=np.float32)
 
         curr_time = message[-1]
         shot = message[-2]
 
-        try:
-            print(f"{1/(curr_time - last_time):.2f}")
-        except:
-            pass
-
         blob_data = message[:-2].reshape(-1, 3)
-
-        last_time = curr_time
-        last_shot = shot
 
 
 except KeyboardInterrupt:
