09b1d9c1194ad371c434040b218cb0f4b15dad4e
Lorenzo Carrera <loolirer@gmail.com>
2025-07-04 22:48:47 -0300

    readd error messages

--- environments/mocaprasp/start.sh
+++ environments/mocaprasp/start.sh
@@ -9,8 +9,8 @@ echo "[INFO] Connected to local network"
 git -C $HOME/VirtualMoCap checkout test > /dev/null 2>&1 || echo "[ERROR] Checkout failed"
 
 # Get updates, if any, from the remote to local repo
-git -C $HOME/VirtualMoCap reset --hard
-git -C $HOME/VirtualMoCap pull
+git -C $HOME/VirtualMoCap reset --hard > /dev/null 2>&1 || echo "[ERROR] Hard reset failed"
+git -C $HOME/VirtualMoCap pull > /dev/null 2>&1 || echo "[ERROR] Pull failed"
 
 # Start pigpiod daemon for advanced GPIO control
 sudo pigpiod > /dev/null 2>&1 || echo "[ERROR] Pigpio daemon failed"
