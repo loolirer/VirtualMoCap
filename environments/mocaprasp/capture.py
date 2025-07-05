b113a2f9e62d3d592c602dd830c70d44c536e047
Lorenzo Carrera <loolirer@gmail.com>
2025-07-04 21:41:17 -0300

    test

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -12,6 +12,13 @@ import numpy as np
 
 from virtualmocap.vision.blob_detection import detect_blobs
 
+# Camera setup
+subprocess.run(
+    ["sudo", "fuser", "-k", "/dev/video0"],  # Kills all video processes
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL,
+)
+
 while True:
     try:
         picam2 = Picamera2()  # Try creating Picamera2 object
