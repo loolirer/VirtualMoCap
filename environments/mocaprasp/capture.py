90bf51d4504ecf1591e418e331ede22d5c21ba71
Lorenzo Carrera <loolirer@gmail.com>
2025-07-09 18:21:31 -0300

    debug

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -80,6 +80,8 @@ def capture_callback(gpio, level, tick):
         shot_number = shot_counter
         shot_counter += 1
 
+    print("AAA")
+
     timestamp = time.time()
     frame = picam2.capture_array()[: resolution[1], : resolution[0]]
 
