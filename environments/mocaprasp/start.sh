0f86a0020956274e07eb3de22cf851df65a41017
Lorenzo Carrera <loolirer@gmail.com>
2025-07-09 18:13:06 -0300

    moved pigpio daemon to install

--- environments/mocaprasp/start.sh
+++ environments/mocaprasp/start.sh
@@ -12,9 +12,6 @@ git -C $HOME/VirtualMoCap checkout test > /dev/null 2>&1 || echo "[ERROR] Checko
 git -C $HOME/VirtualMoCap reset --hard > /dev/null 2>&1 || echo "[ERROR] Hard reset failed"
 git -C $HOME/VirtualMoCap pull > /dev/null 2>&1 || echo "[ERROR] Pull failed"
 
-# Start pigpiod daemon for advanced GPIO control
-sudo pigpiod > /dev/null 2>&1 || echo "[ERROR] Pigpio daemon failed"
-
 # Activate python virtual environment
 source $HOME/.venv/bin/activate > /dev/null 2>&1 || echo "[ERROR] Venv activation failed"
 
