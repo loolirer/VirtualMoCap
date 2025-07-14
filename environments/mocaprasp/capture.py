87b78481ce23e7805ff1b5adb99e00e0025c75b2
Lorenzo Carrera <loolirer@gmail.com>
2025-07-14 16:19:04 -0300

    test

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -34,7 +34,7 @@ picam2.set_controls(
         "AwbEnable": False,
         "ExposureTime": EXPOSURE_TIME,
         "Contrast": 32.0,
-        "AnalogueGain": 10.0
+        "AnalogueGain": 20.0
     }
 )
 
