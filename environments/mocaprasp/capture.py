8f2f567a668f943b3884ae4622dd57758e2c77e1
Lorenzo Carrera <loolirer@gmail.com>
2025-07-14 15:58:36 -0300

    change exposure time

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -30,7 +30,8 @@ picam2.set_controls(
     {  # Set camera controls
         "AwbEnable": False,
         "Contrast": 32.0,
-        "ExposureValue": -2.0,
+        # "ExposureValue": -2.0,
+        "ExposureTime": (1 / 10) * TIME_BUDGET * 1e6,
     }
 )
 
