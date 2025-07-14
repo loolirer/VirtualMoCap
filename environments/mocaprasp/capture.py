117d6db4583014013df16c2134fb6f5f70f32a29
Lorenzo Carrera <loolirer@gmail.com>
2025-07-14 16:13:30 -0300

    change exposure time

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -18,7 +18,7 @@ picam2 = Picamera2()  # Create Picamera2 object
 # Capture parameters
 FPS = 30 # In hertz
 TIME_BUDGET = 1.0 / FPS # In seconds
-EXPOSURE_TIME = 5 # In microseconds
+EXPOSURE_TIME = 5000 # In microseconds
 
 resolution = (960, 720)
 config = picam2.create_video_configuration(
