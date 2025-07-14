492749e345efa62b31e29d7049305b053b7ef24c
Lorenzo Carrera <loolirer@gmail.com>
2025-07-14 17:37:01 -0300

    rollback

--- virtualmocap/vision/blob_detection.py
+++ virtualmocap/vision/blob_detection.py
@@ -28,15 +28,15 @@ params.maxArea = 50
 
 # Filter by Circularity
 params.filterByCircularity = True
-params.minCircularity = 0.5
+params.minCircularity = 0.8
 
 # Filter by Convexity
 params.filterByConvexity = True
-params.minConvexity = 0.5
+params.minConvexity = 0.8
 
 # Filter by Inertia
 params.filterByInertia = True
-params.minInertiaRatio = 0.5
+params.minInertiaRatio = 0.75
 
 # Instanciate marker detector object
 marker_detector = cv2.SimpleBlobDetector_create(params)
