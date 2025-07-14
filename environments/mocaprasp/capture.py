71d5c25ef55b1473abf4a9b12e7b2bf31745c377
Lorenzo Carrera <loolirer@gmail.com>
2025-07-14 17:20:15 -0300

    test

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -99,7 +99,7 @@ def process_and_send():
         except queue.Empty:
             continue
 
-        blobs = detect_blobs(frame, area=True, thresh=127)
+        blobs = detect_blobs(frame, area=True)
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
 
         for b in blobs:
