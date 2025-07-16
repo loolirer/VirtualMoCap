cc75ad680c4943e0c3afb727aa4247d6f79cd338
Lorenzo Carrera <loolirer@gmail.com>
2025-07-16 18:55:24 -0300

    check if element is iterable

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -373,7 +373,18 @@ except KeyboardInterrupt:
 
 except Exception as e:
     print(f"[ERROR] {e}")
-    
+
     for idx, r in enumerate(rows):
+        for v in r.values():
+            try:
+                iter(v)
+                print("GOTCHA!!")
+                print(f"{idx}: {r.values()}")
+
+            except TypeError:
+                continue
+
         if len(r) != 7:
             print(f"{idx}: {r.keys()}")
+
+    print("Did not find any...")
