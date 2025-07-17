f4d751b55116cbe5b4e7584cd5c127bb44b1f7a7
Lorenzo Carrera <loolirer@gmail.com>
2025-07-17 19:07:53 -0300

    test

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -224,7 +224,7 @@ def capture_callback(gpio, level, tick):
 
 # Background image processing and communication
 def process_and_send():
-    cutoff = 100
+    cutoff = 127
     threshold = 10
 
     while True:
