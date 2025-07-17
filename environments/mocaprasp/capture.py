cf64e08f9d01c1136770f408b7181399b9da0c35
Lorenzo Carrera <loolirer@gmail.com>
2025-07-17 01:23:16 -0300

    add blur

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -242,7 +242,7 @@ def process_and_send():
         frame_proc = cv2.convertScaleAbs(
             frame_proc, alpha=255 / (255 - cutoff), beta=0.0
         )
-        frame_proc = cv2.GaussianBlur(frame_proc, (5, 5))
+        frame_proc = cv2.GaussianBlur(frame_proc, (5, 5), 0)
 
         blobs = detect_blobs(
             frame_proc, area=True, thresh=127, detector=marker_detector
