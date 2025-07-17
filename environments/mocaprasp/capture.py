85efd804cdd01ece001d80a4beb081d9f1656049
Lorenzo Carrera <loolirer@gmail.com>
2025-07-17 18:33:22 -0300

    test

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -245,9 +245,7 @@ def process_and_send():
         frame_proc = cv2.convertScaleAbs(
             frame_proc, alpha=255 / (255 - cutoff), beta=0.0
         )
-        frame_proc = cv2.medianBlur(
-            frame_proc, 1
-        )  # cv2.GaussianBlur(frame_proc, (5, 5), 0)
+        cv2.GaussianBlur(frame_proc, (5, 5), 0)
         frame_proc = cv2.equalizeHist(frame_proc)
 
         blobs = detect_blobs(
