cc47004d8ffd9815a7e020fc8e2ee3f0dfaabcb0
Lorenzo Carrera <loolirer@gmail.com>
2025-07-16 23:52:30 -0300

    test

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -237,8 +237,6 @@ def process_and_send():
         
         frame = frame[frame < 127] = 0
         frame = np.clip(2*(frame - 127), 0, 255).astype(np.uint8)
-
-        frame = np.clip(4.0 * (frame - 50.0), 0, 255).astype(np.uint8)
         blobs = detect_blobs(frame, area=True, thresh=127, detector=marker_detector)
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
 
