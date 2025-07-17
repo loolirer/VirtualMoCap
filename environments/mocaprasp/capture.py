b38d9edc857c60838a013e21150a8207daa37633
Lorenzo Carrera <loolirer@gmail.com>
2025-07-17 17:46:41 -0300

    exposure time

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -157,7 +157,7 @@ picam2 = Picamera2()  # Create Picamera2 object
 
 FPS = 30  # In hertz
 TIME_BUDGET = 1.0 / FPS  # In seconds
-EXPOSURE_TIME = 10000  # In microseconds
+EXPOSURE_TIME = 20000  # In microseconds
 
 resolution = (960, 720)
 config = picam2.create_video_configuration(
@@ -172,6 +172,7 @@ picam2.set_controls(
     {  # Set camera controls
         "AeEnable": False,
         "AwbEnable": False,
+        "ExposureTime": EXPOSURE_TIME,
     }
 )
 
