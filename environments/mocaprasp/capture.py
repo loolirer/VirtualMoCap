a9c5b354be265fdbd61f4c8820dcdcce388fb83b
Lorenzo Carrera <loolirer@gmail.com>
2025-07-17 00:43:03 -0300

    test

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -229,7 +229,7 @@ def capture_callback(gpio, level, tick):
 
 def lumizoom(image, cutoff):
     image[image <= 127] = 0
-    return np.clip((255 / (255 - cutoff)) * (image - cutoff), 0, 255).astype(np.uint8)
+    return np.clip(np.round(255 / (255 - cutoff)) * (image - cutoff), 0, 255).astype(np.uint8)
 
     # Cut luminosity values below cutoff
     # image[image < cutoff] = 0
