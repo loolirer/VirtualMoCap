2e12c3da1e51d0d0914099f7973cbb1e3e135351
Lorenzo Carrera <loolirer@gmail.com>
2025-07-17 17:09:44 -0300

    add gaussian

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -242,7 +242,7 @@ def process_and_send():
         frame_proc = cv2.convertScaleAbs(
             frame_proc, alpha=255 / (255 - cutoff), beta=0.0
         )
-        # frame_proc = cv2.GaussianBlur(frame_proc, (5, 5), 0)
+        frame_proc = cv2.GaussianBlur(frame_proc, (3, 3), 0)
 
         blobs = detect_blobs(frame_proc, area=True, thresh=50, detector=marker_detector)
 
