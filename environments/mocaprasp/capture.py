f7663a626ecc820affc011c231aee0a6ff048a10
Lorenzo Carrera <loolirer@gmail.com>
2025-07-18 20:45:53 -0300

    test

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -264,7 +264,6 @@ def process_and_send():
         frame_proc = cv2.convertScaleAbs(
             frame_proc, alpha=255 / (255 - cutoff), beta=0.0
         )
-        frame_proc = cv2.GaussianBlur(frame_proc, (5, 5), 0)
         frame_proc = cv2.equalizeHist(frame_proc)
 
         _, mask = cv2.threshold(frame_proc, cutoff, 255, cv2.THRESH_BINARY)
