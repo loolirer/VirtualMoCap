05a02f9f36065c3aadd39fbc36a1d2c28b6e73c9
Lorenzo Carrera <loolirer@gmail.com>
2025-07-18 20:40:48 -0300

    test

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -270,13 +270,16 @@ def process_and_send():
             frame_proc, 3
         )  # Removes punctual noise that still passes the filter
 
+        _, mask = cv2.threshold(frame_proc, 127, 255, cv2.THRESH_BINARY)
+        frame_proc = cv2.bitwise_and(frame_proc, mask)
+
         # Detect blobs
         # blobs = detect_blobs(
         #    frame_proc, area=True, thresh=threshold, detector=marker_detector
         # )
 
         # Print blobs
-        frame_display = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+        frame_display = cv2.cvtColor(frame_proc, cv2.COLOR_GRAY2RGB)
         # for b in blobs:
         #    cv2.circle(
         #        frame_display,
