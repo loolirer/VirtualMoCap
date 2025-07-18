6f049a4e9e34ecbd1662bee0ca7c09df164de434
Lorenzo Carrera <loolirer@gmail.com>
2025-07-18 20:10:39 -0300

    test

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -154,7 +154,7 @@ def detect_circles(frame):
     min_radius = 1
     max_radius = 50
     min_dist = 2 * min_radius
-    param1 = 50
+    param1 = 40
     param2 = 10
 
     # Detect circles using HoughCircles
