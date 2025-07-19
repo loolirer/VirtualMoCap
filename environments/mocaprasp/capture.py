7c23f5ec36e3de8da8c30775e78b7dfaee796583
Lorenzo Carrera <loolirer@gmail.com>
2025-07-18 21:11:13 -0300

    change min blob distance

--- environments/mocaprasp/capture.py
+++ environments/mocaprasp/capture.py
@@ -125,7 +125,7 @@ params.maxThreshold = (
 )
 
 # Minimum distance between blobs in pixels
-params.minDistBetweenBlobs = 1
+params.minDistBetweenBlobs = 3
 
 # Filter only dark blobs
 params.filterByColor = True
@@ -243,7 +243,7 @@ def process_and_send():
         )
         frame_proc = cv2.GaussianBlur(frame_proc, (5, 5), 0)
         frame_proc = cv2.equalizeHist(frame_proc)
-        
+
         # Detect blobs
         blobs = detect_blobs(
             frame_proc, area=True, thresh=threshold, detector=marker_detector
